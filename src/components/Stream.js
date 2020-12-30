8d09643648d38cf5f22d614f61af2b2827775059
hoklaamc <hoklaam.cheng@gmail.com>
2020-12-30 10:58:35 -0500

    Stream responsiveness

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { TwitchEmbed } from 'react-twitch-embed';
 
 function Stream() {
-  return <TwitchEmbed channel="DwarveSC" id="DwarveSC" theme="dark" muted />;
+  return (
+    <TwitchEmbed
+      channel="DwarveSC"
+      theme="dark"
+      muted
+      width="100%"
+      withChat={false}
+    />
+  );
 }
 
 export default Stream;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,24 +1,25 @@
 import './pages.css';
 import React from 'react';
-import { Image, Typography } from 'antd';
+import { Col, Image, Row, Typography } from 'antd';
 import { Stream } from '../components';
 
 function HomePage() {
   const { Title, Paragraph } = Typography;
   return (
-    <div className="home-content">
-      <Image
-        src="https://img.icons8.com/plasticine/2x/valorant.png"
-        alt="Valorant logo"
-        preview={false}
-      />
-      <Title>UWCCF Valorant</Title>
-      <Paragraph>Dec 28 2020 - Jan 10 2021</Paragraph>
-      <Paragraph strong>
-        A for-fun Valorant tournament with those in the UWCCF community and
-        friends!
-      </Paragraph>
-      {/* <Button
+    <>
+      <div className="home-content">
+        <Image
+          src="https://img.icons8.com/plasticine/2x/valorant.png"
+          alt="Valorant logo"
+          preview={false}
+        />
+        <Title>UWCCF Valorant</Title>
+        <Paragraph>Dec 28 2020 - Jan 10 2021</Paragraph>
+        <Paragraph strong>
+          A for-fun Valorant tournament with those in the UWCCF community and
+          friends!
+        </Paragraph>
+        {/* <Button
         type="primary"
         size="large"
         href="https://forms.gle/KpF5yikaPrdhvUef7"
@@ -28,8 +29,13 @@ function HomePage() {
         Sign up
       </Button>
       <Paragraph>Deadline 11:59pm EST, Sunday Dec 27 2020</Paragraph> */}
-      <Stream />
-    </div>
+      </div>
+      <Row>
+        <Col lg={{ span: 16, offset: 4 }} span={24}>
+          <Stream />
+        </Col>
+      </Row>
+    </>
   );
 }
 
